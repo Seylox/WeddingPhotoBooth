0d5ef8299e69f1e48a72573ff1056d4d3c1cc21d
Bernd Kampl <bernd@itranslate.com>
2017-12-23 18:41:07 +0100

    Add PrintActivity

---
+++ src/main/java/com/example/sony/cameraremote/PrintActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sony.cameraremote
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PrintActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_print)
+    }
+}
