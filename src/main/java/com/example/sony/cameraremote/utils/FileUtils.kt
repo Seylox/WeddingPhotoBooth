e83e25bba75917ea2abe7de6698ef08b597b8659
Bernd Kampl <bernd@itranslate.com>
2018-04-09 20:07:34 +0200

    Move FileUtils into companion object

--- src/main/java/com/example/sony/cameraremote/utils/FileUtils.kt
+++ src/main/java/com/example/sony/cameraremote/utils/FileUtils.kt
@@ -7,53 +7,54 @@ import java.io.IOException
 import java.nio.channels.FileChannel
 
 class FileUtils {
+    companion object {
+        fun copyFileOrDirectory(srcDir: String, dstDir: String) {
+            try {
+                val src = File(srcDir)
+                val dst = File(dstDir, src.name)
 
-    fun copyFileOrDirectory(srcDir: String, dstDir: String) {
-        try {
-            val src = File(srcDir)
-            val dst = File(dstDir, src.name)
+                if (src.isDirectory) {
 
-            if (src.isDirectory) {
+                    val files = src.list()
+                    val filesLength = files.size
+                    for (i in 0 until filesLength) {
+                        val src1 = File(src, files[i]).path
+                        val dst1 = dst.path
+                        copyFileOrDirectory(src1, dst1)
 
-                val files = src.list()
-                val filesLength = files.size
-                for (i in 0 until filesLength) {
-                    val src1 = File(src, files[i]).path
-                    val dst1 = dst.path
-                    copyFileOrDirectory(src1, dst1)
-
+                    }
+                } else {
+                    copyFile(src, dst)
                 }
-            } else {
-                copyFile(src, dst)
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
+
         }
 
-    }
+        @Throws(IOException::class)
+        fun copyFile(sourceFile: File, destFile: File) {
+            if (!destFile.parentFile.exists())
+                destFile.parentFile.mkdirs()
 
-    @Throws(IOException::class)
-    fun copyFile(sourceFile: File, destFile: File) {
-        if (!destFile.parentFile.exists())
-            destFile.parentFile.mkdirs()
-
-        if (!destFile.exists()) {
-            destFile.createNewFile()
-        }
-
-        var source: FileChannel? = null
-        var destination: FileChannel? = null
-
-        try {
-            source = FileInputStream(sourceFile).channel
-            destination = FileOutputStream(destFile).channel
-            destination!!.transferFrom(source, 0, source!!.size())
-        } finally {
-            if (source != null) {
-                source.close()
+            if (!destFile.exists()) {
+                destFile.createNewFile()
             }
-            if (destination != null) {
-                destination.close()
+
+            var source: FileChannel? = null
+            var destination: FileChannel? = null
+
+            try {
+                source = FileInputStream(sourceFile).channel
+                destination = FileOutputStream(destFile).channel
+                destination!!.transferFrom(source, 0, source!!.size())
+            } finally {
+                if (source != null) {
+                    source.close()
+                }
+                if (destination != null) {
+                    destination.close()
+                }
             }
         }
     }
